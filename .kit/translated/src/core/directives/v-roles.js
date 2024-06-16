9cdec01e38c281f4a6a2c3266499f9e2f94c57dd
Caesar Liu <ck9300001@qq.com>
2024-06-16 20:37:45 +0800

    移除kit翻译文件

--- .kit/translated/src/core/directives/v-roles.js
+++
@@ -1,9 +0,0 @@
-import authorizer from '@/core/authorize'
-
-export default {
-  mounted (el, binding) {
-    if (!authorizer.hasRoles(binding.value)) {
-      el.parentNode && el.parentNode.removeChild(el)
-    <#noparse>}</#noparse>
-  <#noparse>}</#noparse>
-<#noparse>}</#noparse>
